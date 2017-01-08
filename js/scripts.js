b67958ac26f7e5d6a114b4a0013b9bbc323ea8c5
Kamila Tomczyk <ka1130@o2.pl>
2017-01-08 20:07:33 +0100

    Add JS matchMedia.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,37 @@
 $(document).ready(function() {
 
-//#hamburger
-	
-	var hamburger = $("#hamburger i");
-	var mainNav = $("#main-nav");
-	var menuList = mainNav.find("ul");
-	var menuElements = mainNav.find("li");
+    //Responsive Menu
+
+    var mobileViewport = window.matchMedia("screen and (max-width: 480px)");
+
+    var hamburger = $("#hamburger i");
+    var mainNav = $("#main-nav");
+    var menuList = mainNav.find("ul");
+    var menuElements = mainNav.find("li");
 
 
-	hamburger.on("click", function(event) {
+    hamburger.on("click", function(event) {
 
-		mainNav.toggleClass("menu-responsive");
-		hamburger.toggleClass("fa-bars").toggleClass("fa-times");
-		menuList.toggleClass("menu-responsive-list");
-		menuElements.show().addClass("menu-responsive-element");
+        mainNav.toggleClass("menu-responsive");
+        hamburger.toggleClass("fa-bars").toggleClass("fa-times");
+        menuList.toggleClass("menu-responsive-list");
+        menuElements.show().addClass("menu-responsive-element");
 
-	});
+    });
 
-	menuElements.on("click", function(event){
-		mainNav.removeClass("menu-responsive");
-		hamburger.addClass("fa-bars").removeClass("fa-times");
-		menuList.removeClass("menu-responsive-list");
-		menuElements.hide();
-	});
+
+    if (mobileViewport.matches) {
+        menuElements.on("click", function(event) {
+            mainNav.removeClass("menu-responsive");
+            hamburger.addClass("fa-bars").removeClass("fa-times");
+            menuList.removeClass("menu-responsive-list");
+            menuElements.hide();
+        });
+    } 
+
+
+
+
 
 
 });
