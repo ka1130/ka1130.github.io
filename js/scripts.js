72824d48034a7746042352730b7f02123ec1146a
Kamila Tomczyk <ka1130@o2.pl>
2017-01-08 18:39:46 +0100

    Stable version of responsive menu.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,24 @@ $(document).ready(function() {
 	
 	var hamburger = $("#hamburger i");
 	var mainNav = $("#main-nav");
+	var menuList = mainNav.find("ul");
+	var menuElements = mainNav.find("li");
+
 
 	hamburger.on("click", function(event) {
 
 		mainNav.toggleClass("menu-responsive");
 		hamburger.toggleClass("fa-bars").toggleClass("fa-times");
+		menuList.toggleClass("menu-responsive-list");
+		menuElements.show().addClass("menu-responsive-element");
 
+	});
+
+	menuElements.on("click", function(event){
+		mainNav.removeClass("menu-responsive");
+		hamburger.addClass("fa-bars").removeClass("fa-times");
+		menuList.removeClass("menu-responsive-list");
+		menuElements.hide();
 	});
 
 
