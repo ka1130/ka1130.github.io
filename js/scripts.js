29f585d1bb95971548677119acb6331d68d4b77a
Kamila Tomczyk <ka1130@o2.pl>
2017-01-08 17:45:03 +0100

    Add .menu-responsive class, display background.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
 
+//#hamburger
+	
+	var hamburger = $("#hamburger");
+	var mainNav = $("#main-nav");
 
+	hamburger.on("click", function(event) {
+
+		mainNav.addClass("menu-responsive");
+
+	});
 
 
 });
